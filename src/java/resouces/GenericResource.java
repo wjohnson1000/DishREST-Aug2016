b41968fb3f8e283e213436493a1c0c7f1b4c2092
SimonHGR <simon@dancingcloudservices.com>
2016-08-09 13:40:53 -0600

    Added query parameter, header, and Paht parameter handling

--- src/java/resouces/GenericResource.java
+++ src/java/resouces/GenericResource.java
@@ -7,8 +7,12 @@ package resouces;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -16,24 +20,43 @@ import javax.ws.rs.core.MediaType;
  *
  * @author simon
  */
-@Path("generic")
+@Path("/generic")
 public class GenericResource {
 
     @GET
+    @Path("/")
     @Produces(MediaType.TEXT_PLAIN)
-    public String getText() {
-        return "Hello rest world!";
+    public String getText(@DefaultValue("12345") @QueryParam("zipcode") String zipcode,
+            @HeaderParam("x-simon") String simon) {
+        System.out.printf("in getText, zipcode is %s, simon is %s\n", zipcode, simon);
+        return "Hello rest world from zipcode " + zipcode + " simon is " + simon;
     }
 
     @GET
-    @Path("extra")
+    @Path("/")
+    @Produces(MediaType.TEXT_HTML)
+    public String getHtml() {
+        return "<HTML><BODY><H1>Hello HTML rest world!</BODY></HTML>";
+    }
+
+    @GET
+    @Path("/extra")
     public String getMoreInfo() {
         return "This is extra stuff";
     }
     
     @DELETE
-    @Path("extra")
+    @Path("/extra")
     public String deleteSomeStuff() {
         return "ouch, you deleted something";
     }
+    
+    
+    @GET
+    @Path("/extra/{thePrimaryKey}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String get123(@PathParam("thePrimaryKey") String pk) {
+        return String.format("Hello this is %s!", pk);
+    }
+
 }
